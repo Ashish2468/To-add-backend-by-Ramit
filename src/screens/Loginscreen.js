d7daa1bd7d3689a28a38af7c8888a833da9520b0
Ashish <ofb911@gmail.com>
2022-01-03 01:18:57 +0530

    backend linked GG

--- src/screens/Loginscreen.js
+++ src/screens/Loginscreen.js
@@ -2,27 +2,56 @@ import React from 'react';
 import {StyleSheet, Text, View, Image, Button} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {TextInput} from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function LoginScreen({navigation}) {
-  const [text, setText] = React.useState('');
+  const [email, setemail] = React.useState('');
   const [Password, setPassword] = React.useState('');
 
+  const sendCred = async () => {
+    fetch('http://10.0.2.2:3000/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: email,
+        pw: Password,
+      }),
+    })
+      .then(res => res.json())
+      .then(async data => {
+        try {
+          await AsyncStorage.setItem('token', data.token);
+          navigation.replace('Homescreen');
+        } catch (e) {
+          console.log('error hai', e);
+          Alert(e);
+        }
+      });
+  };
+
   return (
-    <View
-      style={{flex: 1, backgroundColor: '#2196f3'}}>
+    <View style={{flex: 1, backgroundColor: '#2196f3'}}>
       {/* <Text>Home Screen</Text> */}
 
       <Image
         source={{
           uri: 'https://cdn.educba.com/academy/wp-content/uploads/2019/01/cropped-EDUCBA_LOGO.png',
         }}
-        style={{width: 250, height: 70, marginBottom: 50,marginTop:190, alignSelf: 'center'}}
+        style={{
+          width: 250,
+          height: 70,
+          marginBottom: 50,
+          marginTop: 190,
+          alignSelf: 'center',
+        }}
       />
       <TextInput
         style={{marginHorizontal: 20}}
         label="Email"
-        value={text}
-        onChangeText={text => setText(text)}
+        value={email}
+        onChangeText={email => setemail(email)}
         mode="outlined"
       />
       <TextInput
@@ -48,18 +77,17 @@ export default function LoginScreen({navigation}) {
           //   marginHorizontal:20
           alignSelf: 'center',
         }}
-        onPress={() => navigation.navigate('Detail')}>
-        <Text style={{fontSize: 20,fontWeight: 'bold'}}>Sign IN</Text>
+        onPress={() => sendCred()}>
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Sign IN</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={{
           //   marginTop: 30,
           justifyContent: 'center',
           alignItems: 'center',
-            marginTop: 50,
-          
+          marginTop: 50,
         }}
-        onPress={() => navigation.navigate('Signupscreen')}>
+        onPress={() => navigation.replace('Signupscreen')}>
         <Text>Not Have an account?</Text>
         <View
           style={{
@@ -70,9 +98,11 @@ export default function LoginScreen({navigation}) {
             //   marginHorizontal:20
             alignSelf: 'center',
             justifyContent: 'center',
-          alignItems: 'center',
+            alignItems: 'center',
           }}>
-          <Text style={{fontSize: 20,fontWeight: 'bold',alignSelf: 'center',}}>Sign UP</Text>
+          <Text style={{fontSize: 20, fontWeight: 'bold', alignSelf: 'center'}}>
+            Sign UP
+          </Text>
         </View>
       </TouchableOpacity>
     </View>
