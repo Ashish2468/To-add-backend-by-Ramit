54ee5ef3b64781a03688a27b79e8170bba582829
Ashish <73551845+Ashish2468@users.noreply.github.com>
2022-01-04 12:53:23 +0530

    Removed unwanted content

--- App.js
+++ App.js
@@ -1,75 +1,15 @@
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
+
 import {Provider as PaperProvider} from 'react-native-paper';
-// import {TextInput} from 'react-native-paper';
-// const App = () => {
-//   const [text, setText] = React.useState('');
-//   return (
-//     <PaperProvider>
-//       <SafeAreaView style={{backgroundColor: '#1aff', flex: 1}}>
-//         <Text>Lorem10</Text>
-//         <Text>Lorem10</Text>
-//         <TextInput
-//           label="Email"
-//           value={text}
-//           onChangeText={text => setText(text)}
-//           mode='outlined'
-//         />
-//         <TextInput
-//           label="Password"
-//           value={text}
-//           onChangeText={text => setText(text)}
-//           mode='outlined'
-//         />
-//         <Text>Lorem10</Text>
-//         <Text>Lorem10</Text>
-//         <Text>Lorem10</Text>
-//         <Text>Lorem10</Text>
-//       </SafeAreaView>
-//     </PaperProvider>
-//   );
-// };
 
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
 
 import * as React from 'react';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-// import StackN from './src/navigator/Navigation';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './src/screens/Loginscreen';
 import Signupscreen from './src/screens/Signupscreen';
 import Homescreen from './src/screens/Homescreen';
 import Basescreen from './src/screens/Basescreen';
-// import HomeScreen from './src/screens/HomeScreen';
-
 const Stack = createStackNavigator();
 const screenOptionStyle = {
   headerShown: false,
@@ -100,8 +40,6 @@ export default function App() {
           <Stack.Screen name="HomeScreen" component={Homescreen} />
           <Stack.Screen name="Signupscreen" component={Signupscreen} />
           <Stack.Screen name="Basescreen" component={Basescreen} />
-          {/* <Stack.Screen name="Detail" component={Detail} /> */}
-          {/* <Stack.Screen name="Home" component={Home} /> */}
         </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
